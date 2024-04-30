2c6ed66695881411b9da0e2c0fd15fdf1cafcbfa
Ryan Kmetz <92795341+rmkenv@users.noreply.github.com>
2024-04-30 12:38:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ import requests
 import plotly.express as px
 import stripe
 
+
+st.write("stripe_link", st.secrets["stripe_link"])
+st.write("stripe_api_key", st.secrets["stripe_api_key"])
+
 # Set the page configuration
 st.set_page_config(layout="wide")
 
